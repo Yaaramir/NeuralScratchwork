d49d417b15de1de2695e913eeaa23cad8da6abc4
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-05-31 15:49:58 +0200

    add Layer_Dense backward pass

--- neural_scratchwork.py
+++ neural_scratchwork.py
@@ -14,7 +14,14 @@ class Layer_Dense:
 
     # Forward pass
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
+
+    # Backward pass
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 class Activation_ReLu:
     
