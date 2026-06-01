c2f0bd14d3824ac0a54d135f1734bd08296739d6
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-06-01 12:23:07 +0200

    add momentum to optimizer SGD

--- neural_scratchwork.py
+++ neural_scratchwork.py
@@ -112,19 +112,40 @@ class Activation_Softmax_Loss_CategoricalCrossEntropy():
         self.dinputs = self.dinputs / n_samples
 
 class Optimizer_SGD:
-    def __init__(self, learning_rate=1., decay=0.):
+    def __init__(self, learning_rate=1., decay=0., momentum=0.):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
         self.iterations = 0
+        self.momentum = momentum
 
     def pre_update_params(self):
         if self.decay:
             self.current_learning_rate = self.learning_rate * (1 / (1. + self.decay * self.iterations))
 
     def update_params(self, layer):
-        layer.weights += -self.current_learning_rate * layer.dweights
-        layer.biases += -self.current_learning_rate * layer.dbiases
+
+        if self.momentum:
+
+            if not hasattr(layer, "weight_momentums"):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                layer.bias_momentums = np.zeros_like(layer.biases)
+
+            # Weight updates
+            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+
+            # Bias updates
+            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+
+        else:
+
+            weight_updates = -self.current_learning_rate * layer.dweights
+            bias_updates = -self.current_learning_rate * layer.dbiases
+
+        layer.weights += weight_updates
+        layer.biases += bias_updates
 
     def post_update_params(self):
         self.iterations += 1
@@ -137,7 +158,7 @@ dense1 = Layer_Dense(2, 64)
 activation1 = Activation_ReLu()
 dense2 = Layer_Dense(64, 3)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossEntropy()
-optimizer = Optimizer_SGD(decay=1e-2)
+optimizer = Optimizer_SGD(decay=1e-3, momentum=0.5)
 
 for epoch in range(10001):
 
