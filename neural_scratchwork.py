a9e624db73d5fe659bac2b118cbe83a245d6caa5
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-13 16:51:39 +0200

    add gradients on regularization to layer's backward pass

--- neural_scratchwork.py
+++ neural_scratchwork.py
@@ -35,6 +35,28 @@ class Layer_Dense:
     def backward(self, dvalues):
         self.dweights = np.dot(self.inputs.T, dvalues)
         self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Gradients on regularization
+        # L1 weights
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+
+        # L1 biases
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+
+        # L2 weights
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.weights
+
+        # L2 biases
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+
         self.dinputs = np.dot(dvalues, self.weights.T)
 
 # Activation functions process a neuron's output and decide what value to forward or whether the
