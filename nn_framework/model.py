cc6eedda15815340a77a414366dfa12bc2044032
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-24 16:16:51 +0200

    add 'add_module' method to model

--- nn_framework/model.py
+++ nn_framework/model.py
@@ -6,13 +6,19 @@ class model:
     def __init__(self):
         self.modules = []
 
+    # In training mode, training modules like dropout layers are allowed to forward data
     def training_mode(self):
         self.training_mode = True
         print("Setting model to training mode. Data will be forwarded by training modules.")
 
+    # In training mode, training modules like dropout layers are not allowed to forward data
     def default_mode(self):
         self.training_mode = False
         print("Setting model to default mode. Data will not be forwarded by training modules.")
+
+    # Add a module to the model
+    def add_module(self, module):
+        self.modules.append(module)
 
     def forward(self, inputs):
         x = inputs
