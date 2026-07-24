bfcb27b501c702d843784bd5e430ef96a0c4a6d6
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-24 13:37:04 +0200

    import loss_functions

--- nn_framework/__init__.py
+++ nn_framework/__init__.py
@@ -1,3 +1,4 @@
 from .activation_functions import Activation_ReLu, Activation_Softmax
 from .layers import Layer_Dense, Layer_Dropout
+from .loss_functions import Loss, Loss_CategoricalCrossEntropy
 from .optimizers import Optimizer_SGD, Optimizer_Adagrad, Optimizer_RMSprop, Optimizer_Adam
