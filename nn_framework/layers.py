b5cd8d3df4cba5d5fb8251a0993a2b40fc7a9c10
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-24 11:57:00 +0200

    add nn_framework/layers.py and implement layer classes here

---
+++ nn_framework/layers.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+# Dense / Fully-Connected Layer
+class Layer_Dense:
+
+    # Initialization
+    def __init__(self, n_inputs, n_neurons,
+                 weight_regularizer_l1=0, bias_regularizer_l1=0,
+                 weight_regularizer_l2=0, bias_regularizer_l2=0):
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+        # L1
+        self.weight_regularizer_l1 = weight_regularizer_l1
+        self.bias_regularizer_l1 = bias_regularizer_l1
+        # L2
+        self.weight_regularizer_l2 = weight_regularizer_l2
+        self.bias_regularizer_l2 = bias_regularizer_l2
+
+    # Forward pass
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+    # Backward pass
+    def backward(self, dvalues):
+
+        # Gradient on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # L1
+        if self.weight_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.weights)
+            dL1[self.weights < 0] = -1
+            self.dweights += self.weight_regularizer_l1 * dL1
+        if self.bias_regularizer_l1 > 0:
+            dL1 = np.ones_like(self.biases)
+            dL1[self.biases < 0] = -1
+            self.dbiases += self.bias_regularizer_l1 * dL1
+        # L2
+        if self.weight_regularizer_l2 > 0:
+            self.dweights += 2 * self.weight_regularizer_l2 * self.weights
+        if self.bias_regularizer_l2 > 0:
+            self.dbiases += 2 * self.bias_regularizer_l2 * self.biases
+
+        self.dinputs = np.dot(dvalues, self.weights.T)
+
+# Dropout Layer
+class Layer_Dropout:
+    def __init__(self, dropout_rate):
+        self.success_rate = 1 - dropout_rate
+
+    # Carry out dropout and balance out the rate by multiplie successful outputs accordingly to rate
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.binary_mask = np.random.binomial(n=1, p=self.success_rate, size=inputs.shape) / self.success_rate
+        self.output = inputs * self.binary_mask
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues * self.binary_mask
