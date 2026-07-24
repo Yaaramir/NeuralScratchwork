ba40901767bc7d819cfdba3b305b459010a3c9c8
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-24 13:31:12 +0200

    import optimizers

--- nn_framework/__init__.py
+++ nn_framework/__init__.py
@@ -1,2 +1,3 @@
-from .activation import Activation_ReLu, Activation_Softmax
+from .activation_functions import Activation_ReLu, Activation_Softmax
 from .layers import Layer_Dense, Layer_Dropout
+from .optimizers import Optimizer_SGD, Optimizer_Adagrad, Optimizer_RMSprop, Optimizer_Adam
