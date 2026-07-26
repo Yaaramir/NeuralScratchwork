45cdc60c571c35fdf4ba7ebbbfc2e38cfd2df748
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-26 15:24:02 +0200

    implement Layer_Convolutional class method forward()

--- nn_scratchwork/layers.py
+++ nn_scratchwork/layers.py
@@ -57,55 +57,72 @@ class Layer_Convolutional_2D:
     # Initialization
     def __init__(self,
                  # Number of input channels
-                 n_input_channels: int,
-                 # m and n values for a (m * n) input matrix
-                 input_size: int,
+                 c_in: int,
                  # Kernel / filter size
                  kernel_size: int | tuple[int, int],
                  # Number of kernels / filters
-                 n_output_channels: int,
+                 c_out: int,
                  # Kernal behaviour
                  stride: int = 1, padding: int = 0):
 
-        """Initializes a Convolutional Layer for CNN.
+        """Initializes a Convolutional Layer for CNN. Creates kernels and feature maps.
         
         Args:
             n_input_channels (int): Number of input channels
-            input_size (int | (int, int)): Size m**2 or (m * n) of the input matrix
-            kernel_size (int | (int, int)): Size m**2 or (m * n) of the kernels
+            kernel_size (int | (int, int)): Size h**2 or (h * w) of the kernels
             n_output_channels (int): Number of output channels (number of kernels)
             stride (int): Size of steps a filter moves on a matrix after completing one calculation. Default is 1.
             padding (int): Size of augmented picture frame for kernels to move over. Default is 0."""
 
 
+        self.c_out = c_out
+
         # Set kernel size from either int or tuple
         if isinstance(kernel_size, int):
-            m_kern = n_kern = kernel_size
+            self.h_kern = self.w_kern = kernel_size
         else:
-            m_kern, n_kern = kernel_size
+            self.h_kern, self.w_kern = kernel_size
 
         # Create kernels with weights and biases
-        self.weights = 0.001 * np.random.randn(m_kern, n_kern, n_input_channels, n_output_channels)
-        self.biases = np.zeros(1, n_output_channels)
+        self.weights = 0.001 * np.random.randn(self.h_kern, self.w_kern, c_in, c_out)
+        self.biases = np.zeros((1, c_out))
 
         self.padding = padding
         self.stride = stride
 
-        # Create feature maps
-        if isinstance(input_size, int):
-            m_in = n_in = input_size
-        else:
-            m_in, n_in = input_size
-
-        # Make sure, kernel and input matrix sizes do work well
-        assert (m_in + 2 * padding >= m_kern) and (n_in + 2 * padding >= n_kern), "ERROR: Input matrix is too small for kernel to move over."
-
-        m_out = (m_in - m_kern + 2 * padding) / stride + 1
-        n_out = (n_in - m_kern + 2 * padding) / stride + 1
-        self.feature_maps = np.zeros(m_out, n_out, n_output_channels)
-
     def forward(self, input_matrix):
 
+        # Get input matrix' properties
+        h_in, w_in, c_in = input_matrix.shape
+
+        # Make sure input matrix is large enough / kernel is small enough
+        assert (h_in + 2 * self.padding >= self.h_kern) and (w_in + 2 * self.padding >= self.w_kern), "ERROR: Input matrix is too small for kernel to move over."
+
+        # Create feature maps as output
+        h_out = (h_in - self.h_kern + 2 * self.padding) / self.stride + 1
+        w_out = (w_in - self.w_kern + 2 * self.padding) / self.stride + 1
+        self.feature_maps = np.zeros((h_out, w_out, self.c_out))
+
+        # For feature map
+        for feature_map in range(self.c_out):
+
+            # For every feature
+            for i in range(h_out):
+                for j in range(w_out):
+
+                    # Get patch for kernel calculations
+                    i_start = i * self.stride
+                    i_end = i_start + self.w_kern
+                    j_start = j * self.stride
+                    j_end = j_start + self.w_kern
+
+                    patch = input_matrix[i_start:i_end, j_start:j_end]
+
+                    # Get the right kernel for this feature map
+                    kernel = self.weights[:, :, :, feature_map]
+
+                    # Calculation
+                    self.feature_maps[i, j, feature_map] = np.sum(patch * kernel) + self.biases[0, feature_map]
 
 
 # Dropout Layer
