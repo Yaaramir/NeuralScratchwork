7005cebab3c4f9f51014b283707ddc1035a25e71
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-26 18:43:27 +0200

    update imports

--- nn_scratchwork/__init__.py
+++ nn_scratchwork/__init__.py
@@ -1,5 +1,5 @@
-from .activation_functions import Activation_ReLu, Activation_Softmax
-from .layers import Layer_Dense, Layer_Dropout
-from .loss_functions import Loss, Loss_CategoricalCrossEntropy
-from .model import Model
-from .optimizers import Optimizer_SGD, Optimizer_Adagrad, Optimizer_RMSprop, Optimizer_Adam
+from .activation_functions import *
+from layers import *
+from .loss_functions import *
+from .model import *
+from .optimizers import *
