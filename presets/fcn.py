856165126c4a2d22b3506ecc5e0eaf1cd2b26265
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-28 00:37:42 +0200

    refactor fcn preset

--- presets/fcn.py
+++ presets/fcn.py
@@ -1,11 +1,6 @@
 import neuralscratchwork as scratch
 from nnfs.datasets import spiral_data
-
-# This model was created to test and showcase the usage of simple Fully Connected Networks (FCN).
-# The data is forwarded through Dense / Linear Layers combined with a ReLU. CCE (with integrated
-# Softmax) acts as Loss Function and an Adam Optimizer updates parameters after backwarding.
-# After training a validation dataset is forwarded to compare accuracy and loss of both runs to
-# check the model's generalization and overall results.
+import numpy as np
 
 # General settings
 dot_precision_workaround: bool = True
@@ -13,7 +8,6 @@ default_dtype: str = 'float64'
 random_seed: int = 0
 
 # Create modules
-model = scratch.Model()
 dense_1 = scratch.Layer_Dense(2, 512, weight_regularizer_l2=1e-3, bias_regularizer_l2=1e-3)
 activation_1 = scratch.Activation_ReLu()
 dropout_1 = scratch.Layer_Dropout(0.1)
@@ -24,37 +18,83 @@ dense_3 = scratch.Layer_Dense(512, 3)
 cce = scratch.Loss_CategoricalCrossEntropy()
 adam = scratch.Optimizer_Adam()
 
-# Create model
-model.add_module(dense_1)
-model.add_module(activation_1)
-model.add_module(dropout_1)
-model.add_module(dense_2)
-model.add_module(activation_2)
-model.add_module(dropout_2)
-model.add_module(dense_3)
-model.add_loss_function(cce)
-model.add_optimizer(adam)
-
 # Create data
-n_samples_per_class = 10
+n_samples_per_class = 1000
 n_classes = 3
+
 X_train, y_train = spiral_data(n_samples_per_class, n_classes)
-print(f"X format: {X_train.ndim}: {X_train.shape}")
-print(f"X:\n {X_train}")
-X_val, y_val = spiral_data(100, 3)
+acc_train = loss_train = None
+
+X_val, y_val = spiral_data(n_samples_per_class, n_classes)
 
 # Training
 epochs = 10000
-train_acc, train_loss = model.train(epochs, X_train, y_train)
+#train_acc, train_loss = model.train(epochs, X_train, y_train)
+for epoch in range(epochs + 1):
+
+    # Forward Pass
+    dense_1.forward(X_train)
+    activation_1.forward(dense_1.output)
+    dropout_1.forward(activation_1.output)
+    dense_2.forward(dropout_1.output)
+    activation_2.forward(dense_2.output)
+    dropout_2.forward(activation_2.output)
+    dense_3.forward(dropout_2.output)
+    data_loss = cce.forward(dense_3.output, y_train)
+    reg_loss = cce.regularization_loss(dense_1) + cce.regularization_loss(dense_2) + cce.regularization_loss(dense_3)
+    loss_train = loss = data_loss + reg_loss
+
+    # Evaluation
+    predictions = np.argmax(cce.output, axis=1)
+    if len(y_train.shape) == 2:
+        y_train = np.argmax(y_train, axis=1)
+    acc_train = accuracy = np.mean(predictions == y_train)
+
+    if not epoch % 100:
+        print(f"epoch: {epoch:.3f}, " +
+              f"acc: {accuracy:.3f}, " +
+              f"loss: {loss:.3f} (data_loss: {data_loss}, reg_loss: {reg_loss}), " +
+              f"lr: {adam.current_learning_rate:.6f}")
+
+    # Backward Pass
+    cce.backward(cce.output, y_train)
+    dense_3.backward(cce.dinputs)
+    dropout_2.backward(dense_3.dinputs)
+    activation_2.backward(dropout_2.dinputs)
+    dense_2.backward(activation_2.dinputs)
+    dropout_1.backward(dense_2.dinputs)
+    activation_1.backward(dropout_1.dinputs)
+    dense_1.backward(activation_1.dinputs)
+
+    # Optimizing
+    adam.pre_update_params()
+    adam.update_params(dense_1)
+    adam.update_params(dense_2)
+    adam.update_params(dense_3)
+    adam.post_update_params()
 
 # Validation
-(val_acc, val_loss, data_loss, reg_loss) = model.forward(X_val, y_val)
+dense_1.forward(X_val)
+activation_1.forward(dense_1.output)
+dropout_1.forward(activation_1.output)
+dense_2.forward(dropout_1.output)
+activation_2.forward(dense_2.output)
+dropout_2.forward(activation_2.output)
+dense_3.forward(dropout_2.output)
+data_loss = cce.forward(dense_3.output, y_val)
+reg_loss = cce.regularization_loss(dense_1) + cce.regularization_loss(dense_2) + cce.regularization_loss(dense_3)
+loss_val = data_loss + reg_loss
+
+predictions = np.argmax(cce.output, axis=1)
+if len(y_val.shape) == 2:
+    y_val = np.argmax(y_val, axis=1)
+acc_val = accuracy = np.mean(predictions == y_val)
 
 # Print evaluation
-print(f"{train_acc:.3f} Training Accuracy")
-print(f"{val_acc:.3f} Validation Accuracy ({(val_acc - train_acc):.3f})\n")
+print(f"{acc_train:.3f} Training Accuracy")
+print(f"{acc_val:.3f} Validation Accuracy ({(acc_val - acc_train):.3f})\n")
 
-print(f"{train_loss:.3f} Training Loss")
-print(f"{val_loss:.3f} Validation Loss ({(val_loss - train_loss):.3f})\n")
+print(f"{loss_train:.3f} Training Loss")
+print(f"{loss_val:.3f} Validation Loss ({(loss_val - loss_train):.3f})\n")
 
 print(f"Training: {n_samples_per_class} samples with {n_classes} classes trained in {epochs} epochs.\n")
