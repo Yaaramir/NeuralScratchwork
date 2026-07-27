50a99f0b58bc793b60b2e836322c76f3c50e7eee
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-27 21:59:33 +0200

    implement Binary Cross Entropy (BCE)

--- neuralscratchwork/loss_functions.py
+++ neuralscratchwork/loss_functions.py
@@ -3,13 +3,13 @@ import numpy as np
 
 class Loss:
 
-    # data loss without L1, L2 regularization
+    # Data loss
     def data_loss(self, output, y):
             sample_losses = self.forward(output, y)
             data_loss = np.mean(sample_losses)
             return data_loss
 
-    # L1 and L2 regularization loss
+    # Regularization Loss (L1, L2)
     def regularization_loss(self, layer):
     
             # default = 0
@@ -30,6 +30,25 @@ class Loss:
     
             return regularization_loss
 
+class Loss_BinaryCrossEntropy(Loss):
+
+    def forward(self, y_pred: np.ndarray, y_true: np.ndarray):
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        sample_losses = -(y_true * np.log(y_pred_clipped) + (1 - y_true) * np.log(1 - y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis=1)
+
+        return sample_losses
+
+    def backward(self, dvalues: np.ndarray, y_true: np.ndarray):
+        n_samples = len(dvalues)
+        n_outputs = len(dvalues[0])
+
+        dvalues_clipped = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        self.dinputs = -(y_true / dvalues_clipped - (1 - y_true) / (1 - dvalues_clipped)) / n_outputs
+        self.dinputs = self.dinputs / n_samples
+         
 # Categorical Cross Entropy to calculate on probability predictions. Integrated Softmax!
 class Loss_CategoricalCrossEntropy(Loss):
 
