ceb64ad8980ced7c9b07945255c527020925ddc6
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-27 22:07:21 +0200

    typo

--- neuralscratchwork/__init__.py
+++ neuralscratchwork/__init__.py
@@ -1,6 +1,5 @@
 from .activation_functions import *
-from .data_loader import *
-from layers import *
+from .layers import *
 from .loss_functions import *
 from .model import *
 from .optimizers import *
