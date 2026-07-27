0d4d48d2753bcb9d7a197a77e83db254b49bf45d
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-27 22:10:45 +0200

    "Delete redundant or obsolete files"

--- neuralscratchwork/data_loader.py
+++
@@ -1,7 +0,0 @@
-class Data_Loader():
-
-    def unpickle(file):
-        import pickle
-        with open(file, 'rb') as data:
-            dict = pickle.load(data, encoding='bytes')
-        return dict
