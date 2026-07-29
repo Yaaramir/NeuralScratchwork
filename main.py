2fd4847b0ccf76510945fcb6c62d9dfc194d9fae
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-29 20:20:13 +0200

    call regression model

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import neuralscratchwork as scratch
 
 # FCN for classification tasks
-model_1 = scratch.FCN_Classification()
+#model_1 = scratch.FCN_Classification()
 #model_1.run()
 
 # FCN for binary regression tasks
-model_2 = scratch.FCN_BinaryRegression()
+#model_2 = scratch.FCN_BinaryRegression()
 #model_2.run()
+
+# FCN for regression tasks
+model_3 = scratch.FCN_Regression()
+model_3.run()
