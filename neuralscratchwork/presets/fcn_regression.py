a6abc527fb1b755e239d3396842b07f3fe53ceda
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-29 16:01:13 +0200

    make fcn regression a preset

--- neuralscratchwork/presets/fcn_regression.py
+++ neuralscratchwork/presets/fcn_regression.py
@@ -3,52 +3,56 @@ import neuralscratchwork as scratch
 from nnfs.datasets import sine_data
 import numpy as np
 
-# Create dataset
-X, y = sine_data()
+class FCN_Regression:
 
-# Create modules
-dense_1 = scratch.Layer_Dense(1, 64)
-activation_1 = scratch.Activation_ReLu()
-dense_2 = scratch.Layer_Dense(64, 1)
-activation_2 = scratch.Activation_Linear()
-mse = scratch.Loss_MeanSquaredError()
-adam = scratch.Optimizer_Adam()
+    def run():
 
-accuracy_precision = np.std(y) / 250
+        # Create dataset
+        X, y = sine_data()
 
-# Training
-n_epochs = 10000
-for epoch in range(n_epochs + 1):
+        # Create modules
+        dense_1 = scratch.Layer_Dense(1, 64)
+        activation_1 = scratch.Activation_ReLu()
+        dense_2 = scratch.Layer_Dense(64, 1)
+        activation_2 = scratch.Activation_Linear()
+        mse = scratch.Loss_MeanSquaredError()
+        adam = scratch.Optimizer_Adam()
 
-    # Forward pass
-    dense_1.forward(X)
-    activation_1.forward(dense_1.output)
-    dense_2.forward(activation_1.output)
-    activation_2.forward(dense_2.output)
-    data_loss = mse.data_loss(activation_2.output, y)
-    reg_loss = mse.regularization_loss(dense_1) + mse.regularization_loss(dense_2)
-    loss = data_loss + reg_loss
+        accuracy_precision = np.std(y) / 250
 
-    # Accuracy
-    predictions = activation_2.output
-    acc = np.mean(np.absolute(predictions - y) < accuracy_precision)
+        # Training
+        n_epochs = 10000
+        for epoch in range(n_epochs + 1):
 
-    # Print training progress
-    if not epoch % 100:
-        print(f"epoch: {epoch}, " +
-              f"accuracy: {acc}, " +
-              f"loss: {loss} (data loss: {data_loss}, regularization loss: {reg_loss}), " +
-              f"lr: {adam.current_learning_rate}")
+            # Forward pass
+            dense_1.forward(X)
+            activation_1.forward(dense_1.output)
+            dense_2.forward(activation_1.output)
+            activation_2.forward(dense_2.output)
+            data_loss = mse.data_loss(activation_2.output, y)
+            reg_loss = mse.regularization_loss(dense_1) + mse.regularization_loss(dense_2)
+            loss = data_loss + reg_loss
 
-    # Backward pass
-    mse.backward(activation_2.output, y)
-    activation_2.backward(mse.dinputs)
-    dense_2.backward(activation_2.dinputs)
-    activation_1.backward(dense_2.dinputs)
-    dense_1.backward(activation_1.dinputs)
+            # Accuracy
+            predictions = activation_2.output
+            acc = np.mean(np.absolute(predictions - y) < accuracy_precision)
 
-    # Optimization
-    adam.pre_update_params()
-    adam.update_params(dense_1)
-    adam.update_params(dense_2)
-    adam.post_update_params()
+            # Print training progress
+            if not epoch % 100:
+                print(f"epoch: {epoch}, " +
+                    f"accuracy: {acc}, " +
+                    f"loss: {loss} (data loss: {data_loss}, regularization loss: {reg_loss}), " +
+                    f"lr: {adam.current_learning_rate}")
+
+            # Backward pass
+            mse.backward(activation_2.output, y)
+            activation_2.backward(mse.dinputs)
+            dense_2.backward(activation_2.dinputs)
+            activation_1.backward(dense_2.dinputs)
+            dense_1.backward(activation_1.dinputs)
+
+            # Optimization
+            adam.pre_update_params()
+            adam.update_params(dense_1)
+            adam.update_params(dense_2)
+            adam.post_update_params()
