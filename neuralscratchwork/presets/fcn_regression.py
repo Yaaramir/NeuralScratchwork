91ca6529bec9936c48589f04d4a9283d9dab2183
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-07-29 20:19:52 +0200

    add additional dense layer and activation function

--- neuralscratchwork/presets/fcn_regression.py
+++ neuralscratchwork/presets/fcn_regression.py
@@ -15,6 +15,7 @@ class FCN_Regression:
         X_train, y_train = sine_data()
         X_test, y_test = sine_data()
 
+        # Plot training data
         plt.plot(X_train, y_train)
         plt.suptitle("Training Data")
         plt.show()
@@ -22,14 +23,17 @@ class FCN_Regression:
         # Create modules
         dense_1 = scratch.Layer_Dense(1, 64)
         activation_1 = scratch.Activation_ReLu()
-        dense_2 = scratch.Layer_Dense(64, 1)
-        activation_2 = scratch.Activation_Linear()
+        dense_2 = scratch.Layer_Dense(64, 64)
+        activation_2 = scratch.Activation_ReLu()
+        dense_3 = scratch.Layer_Dense(64, 1)
+        activation_3 = scratch.Activation_Linear()
         mse = scratch.Loss_MeanSquaredError()
-        adam = scratch.Optimizer_Adam()
+        adam = scratch.Optimizer_Adam(learning_rate=0.01, decay=1e-3)
 
         accuracy_precision = np.std(y_train) / 250
 
         # Training
+        loss_train = acc_train = None
         n_epochs = 10000
         for epoch in range(n_epochs + 1):
 
@@ -38,24 +42,28 @@ class FCN_Regression:
             activation_1.forward(dense_1.output)
             dense_2.forward(activation_1.output)
             activation_2.forward(dense_2.output)
-            data_loss = mse.data_loss(activation_2.output, y_train)
-            reg_loss = mse.regularization_loss(dense_1) + mse.regularization_loss(dense_2)
-            loss = data_loss + reg_loss
+            dense_3.forward(activation_2.output)
+            activation_3.forward(dense_3.output)
+            data_loss = mse.data_loss(activation_3.output, y_train)
+            reg_loss = mse.regularization_loss(dense_1) + mse.regularization_loss(dense_2) + mse.regularization_loss(dense_3)
+            loss_train = data_loss + reg_loss
 
             # Accuracy
-            predictions = activation_2.output
-            acc = np.mean(np.absolute(predictions - y_train) < accuracy_precision)
+            predictions = activation_3.output
+            acc_train = np.mean(np.absolute(predictions - y_train) < accuracy_precision)
 
             # Print training progress
-            if not epoch % 100:
+            if not epoch % 1000:
                 print(f"epoch: {epoch}, " +
-                    f"accuracy: {acc}, " +
-                    f"loss: {loss} (data loss: {data_loss}, regularization loss: {reg_loss}), " +
-                    f"lr: {adam.current_learning_rate}")
+                    f"accuracy: {acc_train:.3f}, " +
+                    f"loss: {loss_train:.7f} (data loss: {data_loss:.7f}, regularization loss: {reg_loss:.7f}), " +
+                    f"lr: {adam.current_learning_rate:.5f}")
 
             # Backward pass
-            mse.backward(activation_2.output, y_train)
-            activation_2.backward(mse.dinputs)
+            mse.backward(activation_3.output, y_train)
+            activation_3.backward(mse.dinputs)
+            dense_3.backward(activation_3.dinputs)
+            activation_2.backward(dense_3.dinputs)
             dense_2.backward(activation_2.dinputs)
             activation_1.backward(dense_2.dinputs)
             dense_1.backward(activation_1.dinputs)
@@ -64,15 +72,27 @@ class FCN_Regression:
             adam.pre_update_params()
             adam.update_params(dense_1)
             adam.update_params(dense_2)
+            adam.update_params(dense_3)
             adam.post_update_params()
 
-        # Validation
+        # Testing
         dense_1.forward(X_test)
         activation_1.forward(dense_1.output)
         dense_2.forward(activation_1.output)
         activation_2.forward(dense_2.output)
+        dense_3.forward(activation_2.output)
+        activation_3.forward(dense_3.output)
+
+        # Calculate loss and accuracy
+        loss_test = mse.data_loss(activation_3.output, y_test)
+        acc_test = np.mean(np.absolute(activation_3.output - y_test) < accuracy_precision)
+
+        print(f"\n{loss_train:.3f} Training Loss")
+        print(f"{loss_test:.3f} Testing Loss ({(loss_test - loss_train):.3f})\n")
+        print(f"{acc_train:.3f} Training Accuracy")
+        print(f"{acc_test:.3f} Testing Accuracy ({(acc_test - acc_train):.3f})\n")
 
         plt.plot(X_test, y_test)
-        plt.plot(X_test, activation_2.output)
+        plt.plot(X_test, activation_3.output)
         plt.suptitle("Test Data and Predictions")
         plt.show()
