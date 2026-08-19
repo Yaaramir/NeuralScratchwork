fc2cc7c4bdad30aa75a60e51a0cd467eb7761024
Christopher Hillebrand <christopher.hillebrand@outlook.de>
2026-08-19 16:55:19 +0200

    add tests for Layer_Dense __init__()

---
+++ tests/test_modules/test_layers.py
@@ -0,0 +1,42 @@
+from neuralscratchwork.modules.layers import *
+
+# Layer_Dense
+def test_layer_dense_init_dimensions():
+    """Checks if initialization outputs a layer with correct dimensions of weights and biases."""
+    input_channels = 12
+    n_neurons = 23
+    layer = Layer_Dense(input_channels, n_neurons)
+
+    assert layer.weights.shape == (input_channels, n_neurons), \
+        f"layer.weights.shape was expected to be ({input_channels, n_neurons}) but was {layer.weights.shape}"
+    assert layer.biases.shape == (1, n_neurons), \
+        f"layer.biases.shape was expected to be ({1, n_neurons}) but was {layer.biases.shape}"
+
+def test_layer_dense_regularizer_assignments():
+    """Checks if regularization values are assigned correctly"""
+    input_channels = 12
+    n_neurons = 23
+    weight_regularizer_l1 = 0.1
+    bias_regularizer_l1 = 0.2
+    weight_regularizer_l2 = 0.3
+    bias_regularizer_l2 = 0.4
+    layer = Layer_Dense(input_channels, n_neurons,
+                        weight_regularizer_l1, bias_regularizer_l1,
+                        weight_regularizer_l2, bias_regularizer_l2)
+
+    assert layer.weight_regularizer_l1 == weight_regularizer_l1, \
+        f"layer.weight_regularizer_l1 was expected to be {weight_regularizer_l1} but was {layer.weight_regularizer_l1}"
+    assert layer.bias_regularizer_l1 == bias_regularizer_l1, \
+        f"layer.bias_regularizer_l1 was expected to be {bias_regularizer_l1} but was {layer.bias_regularizer_l1}"
+    assert layer.weight_regularizer_l2 == weight_regularizer_l2, \
+        f"layer.weight_regularizer_l2 was expected to be {weight_regularizer_l2} but was {layer.weight_regularizer_l2}"
+    assert layer.bias_regularizer_l2 == bias_regularizer_l2, \
+        f"layer.bias_regularizer_l2 was expected to be {bias_regularizer_l2} but was {layer.bias_regularizer_l2}"
+
+def test_layer_dense_random_weights():
+    input_channels = 4
+    n_neurons = 2
+    layer = Layer_Dense(input_channels, n_neurons)
+
+    assert not np.all(layer.weights == 0), \
+        f"Weights should not be all zeros"
